ff8d04e5c7f248b5166d1184ee8a5c475dc5db30
rogoyx <rogoyx@gmail.com>
2019-12-01 17:19:03 +0300

    Added a registration form, changed authorization form.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from register import views as v
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', v.register, name='register'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
